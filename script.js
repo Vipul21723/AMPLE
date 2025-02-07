8cea443968afc325404eb0719b027bc14377b186
Vipul Patel <vipul21723@gmail.com>
2025-02-07 07:33:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('redirectButton');
+        
+button.addEventListener('click', function() {
+                window.location.href = 'loading.html';
+        });

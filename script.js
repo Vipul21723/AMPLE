78e629906599b048adc265cb17be1527e0291001
Vipul Patel <vipul21723@gmail.com>
2025-02-09 17:33:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const button = document.getElementById('redirectButton');
         
 button.addEventListener('click', function() {
-                alert("HEllo");
                 window.location.href = 'loading.html';
         });
